cb7e736bdf5eab815d5de11f2d8e6474446734f5
josetx3 <josequintero@wposs.com>
2024-09-26 08:36:46 -0500

    first commit

---
+++ src/main/java/com/jose/pizza/JosePizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.jose.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosePizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosePizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jose/pizza/JosePizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosePizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
